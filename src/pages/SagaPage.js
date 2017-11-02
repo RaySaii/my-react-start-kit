f095c7bec332545957cf00e438705f5010e22869
raysaii <309406931@qq.com>
2017-11-02 19:08:54 +0800

    1.删除预编译 js 文件

--- src/pages/SagaPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PictureList from '../components/PictureList';
-var SagaPage = function () {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "picture list"),
-        React.createElement(PictureList, null)));
-};
-export default SagaPage;
-//# sourceMappingURL=SagaPage.js.map
--- src/pages/TodoPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TodoList from '../components/TodoList';
-var TodoPage = function () {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "tdd"),
-        React.createElement(TodoList, null)));
-};
-export default TodoPage;
-//# sourceMappingURL=TodoPage.js.map
